a1f2e9358ec87dc85c0f4cfa6fb3afd7db9d1e78
osamasy1 <osama.atalla3@gmail.com>
2022-06-21 11:47:23 +0200

    Config gegevens gewijzigd
    connecten met db

--- app/config/config.php
+++ app/config/config.php
@@ -2,11 +2,11 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'magalotus-magazijn');
+define('DB_NAME', 'Autorijschool');
 
 define('APPROOT', dirname(dirname(__FILE__))) ;
 
-define('URLROOT', 'http://www.mvc-framework.org');
+define('URLROOT', 'http://www.autorijschool.nl');
 
 define('SITENAME', 'MVC Framework');
 ?>

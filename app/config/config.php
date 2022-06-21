45f41d6a66e5ea984a0a010cfad7e23c1f1f3a1b
osamasy1 <osama.atalla3@gmail.com>
2022-06-21 11:50:56 +0200

    localhost
    localhost

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,7 @@ define('DB_NAME', 'Autorijschool');
 
 define('APPROOT', dirname(dirname(__FILE__))) ;
 
-define('URLROOT', 'http://www.autorijschool.nl');
+define('URLROOT', 'http://www.autorijschoolgit.nl');
 
 define('SITENAME', 'MVC Framework');
 ?>

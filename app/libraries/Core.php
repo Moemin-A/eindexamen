f8aa9b29b4e7d85e37939257bf23b57bcfe97c27
osamasy1 <osama.atalla3@gmail.com>
2022-06-21 11:53:36 +0200

    controller naam gewijzigd
    controller naam gewijzigd

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -5,7 +5,7 @@
    * URL FORMAT - /controller/method/params
    */
   class Core {
-    protected $currentController = 'ToDo';
+    protected $currentController = 'Overzicht';
     protected $currentMethod = 'index';
     protected $params = [];
 

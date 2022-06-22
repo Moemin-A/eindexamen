9453f2e1c746b336c7c5435203a954c548349544
osamasy1 <osama.atalla3@gmail.com>
2022-06-22 15:05:01 +0200

    functie toevoegen
    functie toevoegen

--- app/models/Gebruiker.php
+++ app/models/Gebruiker.php
@@ -5,28 +5,12 @@
     public function __construct() {
       $this->db = new Database();
 }
+ 
 
-//Hier haal je alle gebruikers van de database en daarna haal je alleen ROL= Instructeurs
-    public function getInstructeurs() {
-      $this->db->query("SELECT * from persoon INNER JOIN rol on persoon.RolId = '3' AND rol.Omschrijving = 'instructeur'");
-
-      $result = $this->db->resultSet();
-
-      return $result;
-}
-
-//Hier haal je alle gebruikers van de database en daarna haal je alleen ROL= leerling
-   public function getLeerlingen(){
-    $this->db->query("SELECT * from persoon INNER JOIN rol on persoon.RolId = '4' AND rol.Omschrijving = 'leerling'");
+//Hier haal je alle AantalLessen, Prijs, BetaalTermijnen van de database waarbij de prijs=400
+   public function getPakket(){
+    $this->db->query("SELECT AantalLessen, Prijs, BetaalTermijnen FROM `pakketten` WHERE Prijs =400");
     return $this->db->resultSet();
-}
-
-//Hier haal je alle gebruikers van de database
-  public function getAll(){
-    $this->db->query("SELECT * from persoon INNER JOIN rol on rol.Id = persoon.RolId ORDER BY Rol ASC");
-    return $this->db->resultSet();
-}
-
+   }
   }
-
 ?>

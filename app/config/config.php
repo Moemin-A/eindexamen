05554b800568e63f90386841db96deee84bb6290
osamasy1 <osama.atalla3@gmail.com>
2022-06-22 15:01:59 +0200

    Database connecten
    Database connecten

--- app/config/config.php
+++ app/config/config.php
@@ -2,11 +2,11 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'Autorijschool');
+define('DB_NAME', 'userstory3');
 
 define('APPROOT', dirname(dirname(__FILE__))) ;
 
-define('URLROOT', 'http://www.autorijschool.nl');
+define('URLROOT', 'http://www.userstory3.nl');
 
 define('SITENAME', 'MVC Framework');
 ?>
